374e6d6af2b2689c5d4c5a223bd75cdf3c15ae4b
John <ski.brimson@gmail.com>
2024-08-07 23:10:13 -0500

    Script to convert to HEVC 1080, used for old MPEG files

--- bin/tran1080.sh
+++ bin/tran1080.sh
@@ -8,8 +8,7 @@ while read f ; do
     # exec >$outdir/log/$f.txt
     echo $f
     echo "HandBrakeCLI -v10 --preset-import-file $jsonfile -i /mnt/bigr0/store/$f -o /home/mythtv/Transcode/pass3/$f"
-    exit
-    if ! HandBrakeCLI --preset-import-file $jsonfile -i /mnt/bigr0/store/$f -o /home/mythtv/Transcode/pass3/$f ; then
+    if ! HandBrakeCLI --preset-import-file $jsonfile -Z "HEVC1080p30" -i /mnt/bigr0/store/$f -o /home/mythtv/Transcode/pass3/$f ; then
         echo $f failed
         echo $f failed >/dev/tty
         exit 1
