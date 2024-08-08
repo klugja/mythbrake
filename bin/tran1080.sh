93100a69c7c02b492956b3703a54f53542be04f4
John <ski.brimson@gmail.com>
2024-08-07 22:54:16 -0500

    Handbrake creates h.264 with x265 settings

---
+++ bin/tran1080.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+flist=$1
+outdir=/home/mythtv/Transcode/pass3
+jsonfile=/usr/local/lib/handbrake/HEVC1080p30.json
+mkdir -p $outdir/log
+exec 2>&1
+while read f ; do
+    # exec >$outdir/log/$f.txt
+    echo $f
+    echo "HandBrakeCLI -v10 --preset-import-file $jsonfile -i /mnt/bigr0/store/$f -o /home/mythtv/Transcode/pass3/$f"
+    exit
+    if ! HandBrakeCLI --preset-import-file $jsonfile -i /mnt/bigr0/store/$f -o /home/mythtv/Transcode/pass3/$f ; then
+        echo $f failed
+        echo $f failed >/dev/tty
+        exit 1
+    fi
+done < $flist
